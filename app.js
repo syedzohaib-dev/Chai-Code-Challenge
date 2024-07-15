0fbd9ff2fad90492bed1b14e5d80010e1a4fce78
syed zohaib <zohaibsyed914@gmail.com>
2024-07-15 17:13:59 +0500

    working-1

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // Chai -- code -- challenge -- day --1
 
+var number = 22
+console.log(number)
+
+let stringMode = "i am happy"
+console.log(stringMode)
+
+const boleanValue = true
+console.log(boleanValue)
+
+let typeCheck = [2, 'fg', true]
+console.log(typeof (typeCheck))
+
+let value1 = "todo"
+let value2 = value1 = "gta"
+console.log(value1)
+console.log(value2)
+
+
+
+
