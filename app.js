93cd199cd5bea473206de705e072a2ddd6196834
syed zohaib <zohaibsyed914@gmail.com>
2024-07-18 17:01:21 +0500

    lesson 3

--- app.js
+++ app.js
@@ -1,21 +1,155 @@
-// Chai -- code -- challenge -- day --1
+// Chai -- code -- challenge -- day --1 _____________________________________
 
-var number = 22
-console.log(number)
+// var number = 22
+// console.log(number)
 
-let stringMode = "i am happy"
-console.log(stringMode)
+// let stringMode = "i am happy"
+// console.log(stringMode)
 
-const boleanValue = true
-console.log(boleanValue)
+// const boleanValue = true
+// console.log(boleanValue)
 
-let typeCheck = [2, 'fg', true]
-console.log(typeof (typeCheck))
+// let typeCheck = [2, 'fg', true]
+// console.log(typeof (typeCheck))
 
-let value1 = "todo"
-let value2 = value1 = "gta"
-console.log(value1)
-console.log(value2)
+// let value1 = "todo"
+// let value2 = value1 = "gta"
+// console.log(value1)
+// console.log(value2)
+
+// let bata = 22
+// const bata = 22
+
+// Chai -- code -- challenge -- day --2 _____________________________________
+
+// let number = 22 + 33
+// console.log(number)
+
+// let minus = 22 - 33
+// console.log(minus)
+
+// let multi = 22 * 33
+// console.log(multi)
+
+// let devide = 22 / 33
+// console.log(devide)
+
+
+// let modulus = 22 % 33
+// console.log(modulus)
+
+// let check = 3>4
+// console.log(check)
+
+// let check = 3 >= 3
+// console.log(check)
+
+// let check = 3 == '3'
+// console.log(check)
+
+// let color = 'red'
+// if(color === 'red' && color === 'white'){
+//     console.log('hello world')
+// }else{
+//     console.log('Wrong')
+// }
+
+// let color = 'red'
+// if (!color) {
+//     console.log('color nhi he')
+// } else {
+//     console.log('color he')
+// }
+
+// let number = 22
+
+// number ? console.log('number he ga') : console.log('number nhi he')
+
+
+// Chai -- code -- challenge -- day --3 _____________________________________
+
+// let number = 2
+// if (number > 0) {
+//     console.log('number is positive')
+// } else if (number < 0) {
+//     console.log('number is negetive')
+// } else if (number == 0) {
+//     console.log('number is 0')
+// } else {
+//     console.log('not a number')
+// }
+
+// let personAge = 12
+// if(personAge >= 18){
+//     console.log('you are elegible to vote')
+// }else{
+//     console.log('you are not eligible')
+// }
+
+// let num1 = 22
+// let num2 = 45
+// let num3 = 42
+
+// if (num1 > num2) {
+//     console.log('num1 is grater')
+//     if (num2 > num3) {
+//         console.log('num2 is grater')
+//     } else {
+//         console.log('num3 is grater')
+//     }
+// } else {
+//     console.log('num2 is grater')
+// }
+
+// let day = 4
+// switch (day) {
+//     case 1:
+//         console.log('monday')
+//         break;
+//     case 2:
+//         console.log('tuesday')
+//         break; case 3:
+//         console.log('wednesday')
+//         break; case 4:
+//         console.log('thursday')
+//         break; case 5:
+//         console.log('friday')
+//         break; case 6:
+//         console.log('saturday')
+//         break; case 7:
+//         console.log('sunday')
+//         break;
+//     default:
+//         break;
+// }
+// let studentTotal = 70
+// switch (studentTotal) {
+//     case 80:
+//         console.log('A')
+//         break;
+//     case 70:
+//         console.log('B')
+//         break;
+//     case 60:
+//         console.log('C')
+//         break;
+//     case 50:
+//         console.log('D')
+//         break;
+//     case 40:
+//         console.log('F')
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let number = 8
+
+// number % 2 === 0 ? console.log('even') : console.log('odd')
+
+
+
 
 
 
